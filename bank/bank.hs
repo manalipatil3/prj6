e8d518313a324f0a9b45d8390e5fd6818e75a8c9
Manali Patil <mpatil3@cs.binghamton.edu>
2024-12-08 17:10:54 -0500

    code fix

--- bank/bank.hs
+++ bank/bank.hs
@@ -1,31 +1,46 @@
 module Bank where
 
-import Control.Monad.State
+-- Define the BankOp type as a state-transforming function
+newtype BankOp a = BankOp { execute :: Float -> (a, Float) }
 
--- Define the BankOp type using State
-newtype BankOp a = BankOp (State Float a)
-  deriving (Functor, Applicative, Monad)
+-- Functor instance for BankOp
+instance Functor BankOp where
+    fmap f (BankOp g) = BankOp $ \s ->
+        let (a, newState) = g s
+        in (f a, newState)
 
--- Function to run a BankOp
-runBankOp :: BankOp a -> Float -> a
-runBankOp (BankOp op) initialBalance = evalState op initialBalance
+-- Applicative instance for BankOp
+instance Applicative BankOp where
+    pure a = BankOp $ \s -> (a, s)
+    (BankOp f) <*> (BankOp g) = BankOp $ \s ->
+        let (func, newState1) = f s
+            (a, newState2) = g newState1
+        in (func a, newState2)
+
+-- Monad instance for BankOp
+instance Monad BankOp where
+    return = pure
+    (BankOp g) >>= f = BankOp $ \s ->
+        let (a, newState) = g s
+            (BankOp h) = f a
+        in h newState
 
 -- Deposit operation
 deposit :: Float -> BankOp ()
-deposit amount = BankOp $ do
-    currentBalance <- get
-    put (currentBalance + amount)
+deposit amount = BankOp $ \balance -> ((), balance + amount)
 
 -- Withdraw operation
 withdraw :: Float -> BankOp Float
-withdraw amount = BankOp $ do
-    currentBalance <- get
-    let actualWithdrawal = if currentBalance - amount < -100
-                           then currentBalance + 100 -- Overdraw up to $100
+withdraw amount = BankOp $ \balance ->
+    let actualWithdrawal = if balance - amount < -100
+                           then balance + 100 -- Allow overdraft up to $100
                            else amount
-    put (currentBalance - actualWithdrawal)
-    return actualWithdrawal
+    in (actualWithdrawal, balance - actualWithdrawal)
 
 -- Get balance operation
 getBalance :: BankOp Float
-getBalance = BankOp get
+getBalance = BankOp $ \balance -> (balance, balance)
+
+-- Function to run BankOp with initial balance of 0.0
+runBankOp :: BankOp a -> a
+runBankOp (BankOp op) = fst (op 0.0)
